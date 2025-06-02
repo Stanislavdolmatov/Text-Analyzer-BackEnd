affc6824540ce12441d6aa636ae47d91ee98baf5
Somos <153925874+Stanislavdolmatov@users.noreply.github.com>
2025-06-02 18:36:11 +0300

    Update uploader.py
    feat

--- uploader.py
+++ uploader.py
@@ -39,8 +39,26 @@ def extract_text(file: UploadFile) -> str:
 
 
 """Добавить код для анализа текста
-def analyze_text(text: str) -> dict:
-"""
+def analyze_text(text: str):
+    d = []
+    p = 'аеёиоуыэюяАЕЁИОУЫЭЮЯ'
+    opa = []
+    tim = re.sub(r'[^\w\s]', '.', text)
+    van = tim.count(".")
+    l = re.sub(r'[^\w\s]', '', text)
+    n = l.split()
+    for i in text.split('.'):
+        r = i.split(' ')
+        r.remove('') if '' in r else None
+        kom = sum(1 for char in l if char in p)
+        opa.append(r)
+        d.append(len(r))
+    sama = kom / len(tim.split())
+    z = float(sum(d)) / len(opa)
+    ma = max(l.split(), key=lambda char: l.split().count(char))
+    mi = min(l.split(), key=lambda char: l.split().count(char))
+    return {"Среднее количество слов": z, "Количество знаков препинания": van,  "количество слов": len(tim.split()),  "самое частое слово": ma,   "самое редкое слово": mi,  "Самое маленькое слово": (sorted(opa, key=len))[0],  "Самое большое слово": (sorted(opa, key=len))[-1], " Количество слогов в среднем": sama}
+
 
 
 @app.post("/upload/", summary="Загрузить и проанализировать текст")
